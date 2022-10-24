8b25d961c0904264fac5cfd9b579766e6924b44f
ReynerBadillaArias <reyner.badilla.arias@est.una.ac.cr>
2022-10-24 13:56:04 -0600

    Particulas

---
+++ convert/src/Components/Convert/Convert.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Convert.css'
+
+const Convert = () => {
+  return (
+    <div className='main-section'>Convert</div>
+  )
+}
+
+export default Convert
--- convert/src/Pages/MainPage/MainPage.jsx
+++ convert/src/Pages/MainPage/MainPage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
+import Footer from '../../Components/Footer/Footer'
+import Convert from '../../Components/Convert/Convert'
+import Particle from '../../Components/Particles/Particle'
 
 const MainPage = () => {
   return (
     <div>
+      <Particle/>
       <Navbar/>
+      <Convert/>
+      <Footer/>
     </div>
   )
 }
